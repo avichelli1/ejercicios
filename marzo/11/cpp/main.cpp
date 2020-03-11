4b7dd235ef9644487ff82aa8ad7709ee378edf70
labopoo <62041344+labopoo@users.noreply.github.com>
2020-03-10 23:43:39 -0300

    Create main.cpp

---
+++ marzo/11/cpp/main.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+#include "ejercicios.h"
+
+using namespace std;
+
+int main() {
+
+    vector<int> a;
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+    imprimirValores(a);
+
+
+    return 0;
+}
