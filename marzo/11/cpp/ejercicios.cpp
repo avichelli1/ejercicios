9da7bc12c57687bca6b1246b6c5b054a1c44d052
labopoo <62041344+labopoo@users.noreply.github.com>
2020-03-10 23:41:53 -0300

    Create ejercicios.cpp

---
+++ marzo/11/cpp/ejercicios.cpp
@@ -0,0 +1,26 @@
+#include "ejercicios.h"
+#include <iostream>
+
+bool esPar(int n){
+    
+}
+
+bool todosSonPares(vector<int> numeros){
+    
+}
+
+void agregarNumeros(vector<int> &numeros, vector<int> elementosNuevos){
+    
+}
+
+void imprimirValores(vector<int> numeros){
+    
+}
+
+vector<vector<int> > multiplicarMatriz(vector<vector<int> > matriz){
+
+}
+
+void mezclarMatrices(vector<vector<int> > matrizA, vector<vector<int> > matrizB){
+
+}
