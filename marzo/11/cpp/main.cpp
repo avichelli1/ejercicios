9e591c0b105deea1af84ffb36e350e41a3297926
labopoo <62041344+labopoo@users.noreply.github.com>
2021-03-29 14:03:32 -0300

    se agregan pruebas para funciones implementadas

--- marzo/11/cpp/main.cpp
+++ marzo/11/cpp/main.cpp
@@ -17,7 +17,43 @@ int main() {
     a.pop_back();
     cout << "tamaño del vector: " << a.size() << endl;
     imprimirValores(a);
+    
+    cout << "prueba para función  esPar" << endl;
 
+    bool valor = esPar(2);
+    cout << valor << endl;
+
+    valor = esPar(1);
+    cout << valor << endl;
+
+    imprimirString("prueba para función todosSonPares");
+
+    vector<int> numeros1;
+    numeros1.push_back(4);
+    numeros1.push_back(2);
+    numeros1.push_back(6);
+
+    bool respuesta = todosSonPares(numeros1);
+    imprimirBool(respuesta);
+
+    numeros1.push_back(1);
+    respuesta = todosSonPares(numeros1);
+    imprimirBool(respuesta);
+
+    imprimirString("prueba con función agregarNumeros");
+
+    vector<int> nuevosNumeros;
+    nuevosNumeros.push_back(7);
+    nuevosNumeros.push_back(8);
+    nuevosNumeros.push_back(3);
+    agregarNumeros(numeros1,nuevosNumeros);
+
+    imprimirValores(numeros1);
+
+    imprimirString("prueba con funcion eliminarImpares");
+
+    eliminarImpares(numeros1);
+    imprimirValores(numeros1);
 
     return 0;
 }
